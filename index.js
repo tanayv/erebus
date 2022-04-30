b97c4eebca525b1dbefd695897aec202af9d691f
Tanay Vardhan <tvardha2@illinois.edu>
2022-04-29 23:57:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import fs from 'fs';
+import https from "https";
+
+const app = express();
+
+const server = https
+    .createServer({
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+        enableTrace: true
+    }, app)
+
+server.listen(4000, () => {
+    console.log('server is runing at port 4000')
+});
+
+// server.on('secureConnection', (socket) => {
+//     //console.log("sc Socket", socket.ssl);
+
+//     //console.log(socket.ssl.requestCert);
+//     console.log(socket.getPeerCertificate());
+// })
+
+app.get('/', (req, res) => {
+    res.json({
+        aurBhai: "SabTheek"
+    })
+})
